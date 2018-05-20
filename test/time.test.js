e6b08e53198cbabb03f6826e32d7447875eae3c8
Timothy Wang <timothywangdev@gmail.com>
2018-05-20 01:50:17 +0000

    Add unit tests for testing timed crowdsale

---
+++ test/time.test.js
@@ -0,0 +1,62 @@
+import ether from 'openzeppelin-solidity/test/helpers/ether';
+import { advanceBlock } from 'openzeppelin-solidity/test/helpers/advanceToBlock';
+import { increaseTimeTo, duration } from 'openzeppelin-solidity/test/helpers/increaseTime';
+import latestTime from 'openzeppelin-solidity/test/helpers/latestTime';
+import EVMRevert from 'openzeppelin-solidity/test/helpers/EVMRevert'
+
+var moment = require("moment")
+const BigNumber = web3.BigNumber;
+
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+const Crowdsale = artifacts.require('Presale');
+const SimpleToken = artifacts.require('Token');
+
+contract('Timed Crowdsale', function ([_, investor, wallet, purchaser]) {
+  const value = ether(42);
+  const tokenSupply = new BigNumber('1e22');
+
+  before(async function () {
+    // Advance to the next block to correctly read time in the solidity "now" function interpreted by ganache
+    await advanceBlock();
+  });
+
+  beforeEach(async function () {
+    this.openingTime = latestTime() + duration.weeks(1);
+    this.closingTime = this.openingTime + duration.weeks(1);
+    this.afterClosingTime = this.closingTime + duration.seconds(1);
+    this.token = await SimpleToken.new();
+    this.crowdsale = await Crowdsale.new(wallet, this.token.address, this.openingTime, this.closingTime);
+    await this.token.transfer(this.crowdsale.address, tokenSupply);
+
+    // whitelist investor and purchaser addresses
+    await this.crowdsale.addToWhitelist(investor, 5, 10000);
+    await this.crowdsale.addToWhitelist(purchaser, 5, 10000);
+  });
+
+  it('should be ended only after end', async function () {
+    let ended = await this.crowdsale.hasClosed();
+    ended.should.equal(false);
+    await increaseTimeTo(this.afterClosingTime);
+    ended = await this.crowdsale.hasClosed();
+    ended.should.equal(true);
+  });
+
+  describe('accepting payments', function () {
+    it('should reject payments before start', async function () {
+      await this.crowdsale.buyTokens(investor, { from: purchaser, value: value }).should.be.rejectedWith(EVMRevert);
+    });
+    it('should accept payments after start', async function () {
+      await increaseTimeTo(this.openingTime);
+      await this.crowdsale.buyTokens(investor, { value: value, from: purchaser }).should.be.fulfilled;
+    });
+
+    it('should reject payments after end', async function () {
+      await increaseTimeTo(this.afterClosingTime);
+      await this.crowdsale.buyTokens(investor, { value: value, from: purchaser }).should.be.rejectedWith(EVMRevert);
+    });
+  });
+});
