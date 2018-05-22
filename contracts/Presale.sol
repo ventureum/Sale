0e85d5bd7338517dcf44a334340c3df2988e1aa8
CHANGLONG ZHONG <vincentzhongca@gmail.com>
2018-05-22 11:47:02 -0400

    Apply Pausable to Sale Contract
    1: Applied Pausable from
    https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/lifecycle/Pausable.sol to Sale contract
    2: Fixed some styling issues.
    3: Improved documentation and styling of Presale.sol

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -4,12 +4,24 @@ import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
-contract Presale is Crowdsale, Ownable {
+contract Presale is Crowdsale, Pausable {
 
-    event Finalized();
 
     using SafeMath for uint256;
+
+    event _Finalized();
+    event _NewWhiteListee(
+        address indexed msgSender,
+        address indexed beneficiary, 
+        uint rate, 
+        uint lockupDuration
+    );
+    event _WhiteListeeRemoved(
+        address indexed msgSender,
+        address indexed beneficiary
+    );
 
     struct WhitelistData {
         bool whitelisted;
@@ -27,11 +39,23 @@ contract Presale is Crowdsale, Ownable {
     mapping(address => uint256) public balances;
 
     /**
-     * @dev Constructor, takes crowdsale opening and closing times.
+     * @dev Constructor, takes crowdsale opening and closing times
+     * @param _wallet Address where funds are collected
+     * @param _token The token being sold
      * @param _openingTime Crowdsale opening time
      * @param _closingTime Crowdsale closing time
      */
-    constructor(address _wallet, ERC20 _token, uint256 _openingTime, uint256 _closingTime) Crowdsale(1, _wallet, _token) Ownable() public {
+    constructor(
+        address _wallet, 
+        ERC20 _token, 
+        uint256 _openingTime, 
+        uint256 _closingTime
+    )
+        public 
+        Crowdsale(1, _wallet, _token) 
+        Ownable() 
+        
+    {
         // solium-disable-next-line security/no-block-members
         require(_openingTime >= block.timestamp);
         require(_closingTime >= _openingTime);
@@ -42,9 +66,10 @@ contract Presale is Crowdsale, Ownable {
 
     /**
      * @dev Reverts if beneficiary is not whitelisted. Can be used when extending this contract.
+     * @param beneficiary the address being checked 
      */
-    modifier isWhitelisted(address _beneficiary) {
-        require(whitelist[_beneficiary].whitelisted);
+    modifier isWhitelisted(address beneficiary) {
+        require(whitelist[beneficiary].whitelisted);
         _;
     }
 
@@ -67,51 +92,94 @@ contract Presale is Crowdsale, Ownable {
     }
 
     /**
-     * @dev Adds single address to whitelist.
-     * @param beneficiary Address to be added to the whitelist
+     * @notice This function adds a single address to whitelist.
+     * @param beneficiary Address to be whitelisted
+     * @param rate The rate to be used at time of purchase for the beneficiary
+     * @param lockupDuration The amount of blocks the beneficiary 
+     *  needs to wait before tokens can be withdrawn
      */
-    function addToWhitelist(address beneficiary, uint rate, uint lockupDuration) public onlyOwner {
+    function addToWhitelist(
+        address beneficiary, 
+        uint rate, 
+        uint lockupDuration
+    )
+        public 
+        onlyOwner 
+        whenNotPaused 
+    {
         WhitelistData storage data = whitelist[beneficiary];
         data.whitelisted = true;
         data.rate = rate;
         data.lockupDuration = lockupDuration;
+
+        emit _NewWhiteListee(msg.sender, beneficiary, rate, lockupDuration);
     }
 
     /**
-     * @dev Adds list of addresses to whitelist. Not overloaded due to limitations with truffle testing.
-     * @param beneficiaries Addresses to be added to the whitelist
+     * @notice This function adds a list of addresses to whitelist
+     * @param beneficiaries Addresses to be whitelisted
+     * @param rate The rate to be used at time of purchase for the beneficiary
+     * @param lockupDuration The amount of blocks the beneficiary 
+     *  needs to wait before tokens can be withdrawn
      */
-    function addManyToWhitelist(address[] beneficiaries, uint[] rate, uint[] lockupDuration) external onlyOwner {
+    function addManyToWhitelist(
+        address[] beneficiaries, 
+        uint[] rate, 
+        uint[] lockupDuration
+    ) 
+        external 
+        onlyOwner 
+        whenNotPaused 
+    {
         for (uint256 i = 0; i < beneficiaries.length; i++) {
             addToWhitelist(beneficiaries[i], rate[i], lockupDuration[i]);
+
+            emit _NewWhiteListee(msg.sender, beneficiaries[i], rate[i], lockupDuration[i]);
         }
     }
 
     /**
-     * @dev Removes single address from whitelist.
-     * @param _beneficiary Address to be removed to the whitelist
+     * @dev Removes single address from whitelist
+     * @param beneficiary Address to be removed from the whitelist
      */
-    function removeFromWhitelist(address _beneficiary) external onlyOwner {
-        whitelist[_beneficiary].whitelisted = false;
+    function removeFromWhitelist(address beneficiary) external onlyOwner whenNotPaused {
+        delete whitelist[beneficiary];
+
+        emit _WhiteListeeRemoved(msg.sender, beneficiary);
     }
 
     // Set the closingTime to block.timestamp and close the sale
-    function finalize() external onlyOwner {
+    function finalize() external onlyOwner whenNotPaused {
 
         /* solium-disable-next-line security/no-block-members */
         closingTime = block.timestamp;
 
-        emit Finalized();
+        emit _Finalized();
     }
 
-    // Set the closingTime to block.timestamp and close the sale
-    function setOpeningAndClosingTime(uint256 _openingTime, uint256 _closingTime) external onlyOwner {
+    /*
+     * @notic Set the closingTime to block.timestamp and close the sale
+     * @dev can only be called by owner and while not paused
+     * @param _openingTime block number that the sale starts
+     * @param _closingTime block number that the sale ends
+     */
+    function setOpeningAndClosingTime(
+        uint256 _openingTime, 
+        uint256 _closingTime
+    ) 
+        external 
+        onlyOwner 
+        whenNotPaused 
+    {
         openingTime = _openingTime;
         closingTime = _closingTime;
     }
 
-    // transfer out tokens
-    function transferTokens(address beneficiary, uint amount) external onlyOwner {
+    /* @notice transfer out tokens
+     * @param beneficiary the address of beneficiary
+     * @param amount the amount of token to be transftered
+     */
+    function transferTokens(address beneficiary, uint amount) external onlyOwner whenNotPaused {
         require(amount <= token.balanceOf(address(this)));
         require(token.transfer(beneficiary, amount));
     }
@@ -119,7 +187,7 @@ contract Presale is Crowdsale, Ownable {
     /**
      * @dev Withdraw tokens only after crowdsale ends.
      */
-    function withdrawTokens() public {
+    function withdrawTokens() public whenNotPaused {
         require(hasClosed());
         require(whitelist[msg.sender].whitelisted);
 
@@ -132,73 +200,90 @@ contract Presale is Crowdsale, Ownable {
         _deliverTokens(msg.sender, amount);
     }
 
-    function buyTokens(address _beneficiary) public payable {
+    /**
+     * @notice buy tokens for beneficiary
+     * @dev low level token purchase 
+     * @param beneficiary Address performing the token purchase
+     */
+    function buyTokens(address beneficiary) public payable onlyWhileOpen whenNotPaused {
 
         uint256 weiAmount = msg.value;
-        _preValidatePurchase(_beneficiary, weiAmount);
+        _preValidatePurchase(beneficiary, weiAmount);
 
         // calculate token amount to be created
-        uint256 tokens = _getTokenAmount(_beneficiary, weiAmount);
+        uint256 tokens = _getTokenAmount(beneficiary, weiAmount);
 
         // update state
         weiRaised = weiRaised.add(weiAmount);
 
-        _processPurchase(_beneficiary, tokens);
-        emit TokenPurchase
-            (
+        _processPurchase(beneficiary, tokens);
+        emit TokenPurchase(
             msg.sender,
-            _beneficiary,
+            beneficiary,
             weiAmount,
             tokens
-            );
+        );
 
-        _updatePurchasingState(_beneficiary, weiAmount);
+        _updatePurchasingState(beneficiary, weiAmount);
 
         _forwardFunds();
-        _postValidatePurchase(_beneficiary, weiAmount);
+        _postValidatePurchase(beneficiary, weiAmount);
+    }
+
+    /*
+     * @notice Purchase token for msg.sender itself
+     * @dev This function is to accomodate front-end implmentation
+     */
+    function purchaseToken() external payable onlyWhileOpen whenNotPaused {
+        buyTokens(msg.sender);
     }
 
     /**
      * @dev Override to extend the way in which ether is converted to tokens.
-     * @param _weiAmount Value in wei to be converted into tokens
-     * @return Number of tokens that can be purchased with the specified _weiAmount
+     * @param beneficiary The address of the beneficiary
+     * @param weiAmount Value in wei to be converted into tokens
+     * @return Number of tokens that can be purchased with the specified weiAmount
      */
-    function _getTokenAmount(address _beneficiary, uint256 _weiAmount)
-        internal view returns (uint256)
+    function _getTokenAmount(
+        address beneficiary, 
+        uint256 weiAmount
+    )
+        internal 
+        view 
+        returns (uint256)
     {
-        return _weiAmount.mul(whitelist[_beneficiary].rate);
+        return weiAmount.mul(whitelist[beneficiary].rate);
     }
 
     /**
      * @dev Overrides parent by storing balances instead of issuing tokens right away.
-     * @param _beneficiary Token purchaser
-     * @param _tokenAmount Amount of tokens purchased
+     * @param beneficiary Token purchaser
+     * @param tokenAmount Amount of tokens purchased
      */
-    function _processPurchase
-       (
-        address _beneficiary,
-        uint256 _tokenAmount
+    function _processPurchase(
+        address beneficiary,
+        uint256 tokenAmount
        )
         internal
+        whenNotPaused
     {
-        balances[_beneficiary] = balances[_beneficiary].add(_tokenAmount);
-        totalTokenSold = totalTokenSold.add(_tokenAmount);
+        balances[beneficiary] = balances[beneficiary].add(tokenAmount);
+        totalTokenSold = totalTokenSold.add(tokenAmount);
     }
 
     /**
      * @dev Extend parent behavior requiring beneficiary to be in whitelist.
-     * @param _beneficiary Token beneficiary
-     * @param _weiAmount Amount of wei contributed
+     * @param beneficiary Token beneficiary
+     * @param weiAmount Amount of wei contributed
      */
-    function _preValidatePurchase
-       (
-        address _beneficiary,
-        uint256 _weiAmount
-       )
+    function _preValidatePurchase(
+        address beneficiary,
+        uint256 weiAmount
+    )
         internal
-        isWhitelisted(_beneficiary)
+        isWhitelisted(beneficiary)
         onlyWhileOpen()
     {
-        super._preValidatePurchase(_beneficiary, _weiAmount);
+        super._preValidatePurchase(beneficiary, weiAmount);
     }
 }
