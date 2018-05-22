9aab3fcbcdfc50359566f2be8df2d95f1ff3fe04
Timothy Wang <timothywangdev@gmail.com>
2018-05-22 01:24:44 +0000

    Use truffle hd wallet in dev env

--- truffle.js
+++ truffle.js
@@ -18,6 +18,13 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
+      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545', 0, 10),
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 5000000
+    },
+    testing: {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*', // Match any network id
